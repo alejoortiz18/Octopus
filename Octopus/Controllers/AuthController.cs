0a130670dc5b5b7ec0337de5745c57422e107a06
alejoortiz18 <45045371+alejoortiz18@users.noreply.github.com>
2025-06-05 20:37:22 -0500

    Agregar css, js, y pagina inicio al proyecto

---
+++ Octopus/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Octopus.Controllers
+{
+    public class AuthController : Controller
+    {
+        
+        public IActionResult InicioSesion()
+        {
+            
+            return View();
+        }
+    }
+}
