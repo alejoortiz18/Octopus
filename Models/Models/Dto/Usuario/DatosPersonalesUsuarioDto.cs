f5cc069e9d625650bef411c6ae036f60d7fa9207
alejoortiz18 <45045371+alejoortiz18@users.noreply.github.com>
2025-06-21 10:00:44 -0500

    Crear mensaje si no hay codigo referencia

--- Models/Models/Dto/Usuario/DatosPersonalesUsuarioDto.cs
+++ Models/Models/Dto/Usuario/DatosPersonalesUsuarioDto.cs
@@ -1,17 +1,20 @@
 ﻿using Models.Entities.Domain.DBOctopus.OctopusEntities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models.Dto.Usuario
 {
     public class DatosPersonalesUsuarioDto
     {
-
         public int Rol { get; set; }
-        public string CodigoReferencia { get; set; }="";
-        public int EstadoUsuario { get; set; }        
+        public string CodigoReferencia { get; set; } = "";
+        public int EstadoUsuario { get; set; }
         public int TipoDocumento { get; set; }
         public string NumeroDocumento { get; set; } = "";
         public string NombreCompleto { get; set; } = null!;
         public string Email { get; set; } = null;
+
+        [Display(Name = "Número Celular")]
+        public string NumeroCelular { get; set; } = null;
         public DateTime FechaRegistro { get; set; }
         public DateTime FechaHabilitacion { get; set; }
     }
