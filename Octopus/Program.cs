f72cadcced40d4f26cb98ec5df379ee09e0bdb55
alejoortiz18 <45045371+alejoortiz18@users.noreply.github.com>
2025-06-24 23:46:47 -0500

    crear el boton para salir o cerrar la sesion

--- Octopus/Program.cs
+++ Octopus/Program.cs
@@ -39,7 +39,7 @@ app.Use(async (context, next) =>
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Index");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
